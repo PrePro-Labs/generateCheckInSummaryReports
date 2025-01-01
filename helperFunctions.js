5aac3473a9820064960d0522a033ba973dfc3672
johngaynor <86327580+johngaynor@users.noreply.github.com>
2025-01-01 13:44:51 -0500

    added function to calculate averages

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,6 +1,6 @@
 const { mysqlPool } = require("./config/database");
 
-async function getPreproLogs(userId, startDate, endDate) {
+async function getSleepLogs(userId, startDate, endDate) {
   const pool = await mysqlPool;
   const [result] = await pool.query(
     `
@@ -15,10 +15,65 @@ async function getPreproLogs(userId, startDate, endDate) {
   return result;
 }
 
-function calculateAverages() {}
+function calculateAverages(logs) {
+  const totals = logs.reduce(
+    (acc, log) => {
+      acc.count += 1;
+      acc.totalSleep += parseFloat(log.totalSleep);
+      acc.totalBed += parseFloat(log.totalBed);
+      acc.recoveryIndex += log.recoveryIndex;
+      acc.remQty += parseFloat(log.remQty);
+      acc.deepQty += parseFloat(log.deepQty);
+      return acc;
+    },
+    {
+      count: 0,
+      totalSleep: 0,
+      totalBed: 0,
+      recoveryIndex: 0,
+      remQty: 0,
+      deepQty: 0,
+    }
+  );
 
-function generateSummaries() {}
+  if (totals.count === 0) {
+    return {};
+  }
+
+  return Object.fromEntries(
+    Object.entries(totals)
+      .filter(([key]) => key !== "count")
+      .map(([key, value]) => [
+        `avg${key.charAt(0).toUpperCase()}${key.slice(1)}`,
+        (value / totals.count).toFixed(2),
+      ])
+  );
+}
+
+async function getPastCheckIns(userId, date) {
+  const pool = await mysqlPool;
+  const [result] = await pool.query(
+    `
+      select 
+        avgTotalSleep,
+        avgTotalBed,
+        avgRecoveryIndex,
+        avgRemQty,
+        avgDeepQty
+      from checkIns
+      where userId = ?
+      and date < ?
+      order by date desc
+      limit 4
+      `,
+    [userId, date]
+  );
+
+  return result;
+}
+
+function generateSummaries(averages) {}
 
 function uploadToPrepro() {}
 
-module.exports = { getPreproLogs };
+module.exports = { getSleepLogs, calculateAverages, getPastCheckIns };
--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const { getPreproLogs } = require("./helperFunctions");
+const {
+  getSleepLogs,
+  calculateAverages,
+  getPastCheckIns,
+} = require("./helperFunctions");
 
 async function main() {
   try {
     console.log("---------- STARTING PROCESS ----------");
 
-    // const userId = params.userId;
     const userId = process.env.USER_ID;
     const startDate = "2024-12-20";
     const endDate = "2024-12-27";
+
+    // Getting prepro logs
     console.log("Getting prepro logs", new Date());
-    const logs = await getPreproLogs(userId, startDate, endDate);
-    console.log(logs);
+    const logs = await getSleepLogs(userId, startDate, endDate);
+    console.log(`Got logs for ${logs.length} days`, new Date());
+
+    // Calculating averages since last checkin
+    console.log("Calculating averages", new Date());
+    const averages = calculateAverages(logs);
+    if (!Object.keys(averages).length === 0) {
+      throw new Error("No logs within specified range");
+    }
+    console.log("Averages calculated", new Date());
+
+    // Retrieving last 4 checkin results
+    console.log("Retrieving historical checkin data", new Date());
+    const historical = await getPastCheckIns(userId, endDate);
+    console.log(
+      `Retrieved historical data for ${historical.length} checkins`,
+      new Date()
+    );
+
+    // Generating summary of data
+    const reports = [{ ...averages, date: endDate }, ...historical];
+    console.log("Generating AI summary of sleep", new Date());
 
     console.log("---------- ENDING PROCESS (success) ----------");
     process.exit();
